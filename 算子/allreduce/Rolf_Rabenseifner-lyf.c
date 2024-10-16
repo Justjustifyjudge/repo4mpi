cf721158f0cbdb63306b7e42d1c01810acc77e53
Justjustifyjudge <1911160724@qq.com>
2024-10-16 11:37:29 +0800

    '最后一个R氏算子尝试，不过未完成'

--- 算子/allreduce/Rolf_Rabenseifner-lyf.c
+++ 算子/allreduce/Rolf_Rabenseifner-lyf.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+int nearest_power_of_two(int x) {
+    int res=1;
+    for(;x>1;res<<=1,x>>=1);
+    return res;
+}
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
@@ -20,6 +25,41 @@ int main(int argc, char** argv) {
         data[i]=rand()%20;
     }
 
+    int used_rank=0;
+    for(;used_rank<num_nodes;){
+        int size=nearest_power_of_two(num_nodes-used_rank);
+        //第一个rank
+        rankfirst=used_rank;
+        ranklast=used_rank+size-1;
+        if(rank>=used_rank&&rank<=used_rank+size-1){
+            //判断是否只有一个节点
+                //只有一个节点，不参与reduce操作
+            if(size==1){
+                continue;
+            } else{
+                float* temp=malloc(size*sizeof(float));
+                //判断是留下来的计算节点还是被合并的计算节点
+                    //留下来的节点
+                if((rank-used_rank)%2==0){
+                    
+                    MPI_Recv(temp,size,MPI_FLOAT,rank+1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+                    for(int i=0;i<total_data_size;i++){
+                        data[i]+=temp[i];
+                    }
+                }
+                    //被合并的节点
+                else{
+                    
+                    for(int i=0;i<total_data_size;i++){
+                        temp[i]=data[i];
+                    }
+                    MPI_Send(temp,size,MPI_FLOAT,rank-1,0,MPI_COMM_WORLD);
+                }
+                free(temp);
+            }
+        }
+        used_rank+=size;
+    }
 
 
     //结束和释放
