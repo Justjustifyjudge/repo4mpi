75fdfae197ae964b7e4dd3d1b33b06da0c2d2ec6
Justjustifyjudge <1911160724@qq.com>
2024-10-31 23:35:42 +0800

    '优化了ring_time中scatter_reduce中分配缓冲区的方法'

--- test/ring_time.c
+++ test/ring_time.c
@@ -31,20 +31,52 @@ int read_data_from_file(const char *filename, float **data) {
     return count;  // 返回浮点数的总数
 }
 
+// void scatter_reduce(float *data, float *recv_buf, float *send_to_buf, int num_nodes, int data_size_per_node, int total_data_size) {
+//     int rank;
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+//     // 从全局数据中分配数据区
+//     for (int i = 0; i < data_size_per_node; i++) {
+//         recv_buf[i] = (rank * data_size_per_node + i < num_nodes * data_size_per_node) ? data[rank * data_size_per_node + i] : 0;
+//     }
+
+//     for (int step = 0; step < num_nodes - 1; step++) {
+//         int send_to = (rank + 1 + step) % num_nodes;
+//         float *send_to_buf_singal = (float *)malloc(data_size_per_node * sizeof(float));
+//         for (int i = 0; i < data_size_per_node; i++) {
+//             send_to_buf_singal[i] = (((rank + step + 1) % num_nodes) * data_size_per_node + i < num_nodes * data_size_per_node)
+//                                       ? data[((rank + step + 1) % num_nodes) * data_size_per_node + i] : 0;
+//         }
+
+//         int recv_from = (rank - 1 + num_nodes - step) % num_nodes;
+//         float *temp_buf = (float *)malloc(data_size_per_node * sizeof(float));
+//         MPI_Status status;
+
+//         MPI_Send(send_to_buf_singal, data_size_per_node, MPI_FLOAT, send_to, 0, MPI_COMM_WORLD);
+//         MPI_Recv(temp_buf, data_size_per_node, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD, &status);
+
+//         for (int i = 0; i < data_size_per_node; i++) {
+//             recv_buf[i] += temp_buf[i];
+//         }
+
+//         free(temp_buf);
+//     }
+// }
+
 void scatter_reduce(float *data, float *recv_buf, float *send_to_buf, int num_nodes, int data_size_per_node, int total_data_size) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // 从全局数据中分配数据区
     for (int i = 0; i < data_size_per_node; i++) {
-        recv_buf[i] = (rank * data_size_per_node + i < num_nodes * data_size_per_node) ? data[rank * data_size_per_node + i] : 0;
+        recv_buf[i] = (rank * data_size_per_node + i < total_data_size) ? data[rank * data_size_per_node + i] : 0;
     }
 
     for (int step = 0; step < num_nodes - 1; step++) {
         int send_to = (rank + 1 + step) % num_nodes;
         float *send_to_buf_singal = (float *)malloc(data_size_per_node * sizeof(float));
         for (int i = 0; i < data_size_per_node; i++) {
-            send_to_buf_singal[i] = (((rank + step + 1) % num_nodes) * data_size_per_node + i < num_nodes * data_size_per_node)
+            send_to_buf_singal[i] = (((rank + step + 1) % num_nodes) * data_size_per_node + i < total_data_size)
                                       ? data[((rank + step + 1) % num_nodes) * data_size_per_node + i] : 0;
         }
 
@@ -52,16 +84,18 @@ void scatter_reduce(float *data, float *recv_buf, float *send_to_buf, int num_no
         float *temp_buf = (float *)malloc(data_size_per_node * sizeof(float));
         MPI_Status status;
 
-        MPI_Send(send_to_buf_singal, data_size_per_node, MPI_FLOAT, send_to, 0, MPI_COMM_WORLD);
-        MPI_Recv(temp_buf, data_size_per_node, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD, &status);
+        MPI_Send(send_to_buf_singal, data_size_per_node, MPI_FLOAT, send_to, step, MPI_COMM_WORLD);
+        MPI_Recv(temp_buf, data_size_per_node, MPI_FLOAT, recv_from, step, MPI_COMM_WORLD, &status);
 
         for (int i = 0; i < data_size_per_node; i++) {
             recv_buf[i] += temp_buf[i];
         }
 
         free(temp_buf);
+        free(send_to_buf_singal);  // 确保释放内存
     }
 }
+
 
 void allgather(float *recv_buf, int num_nodes, int data_size_per_node) {
     float *gathered_data = (float *)malloc(num_nodes * data_size_per_node * sizeof(float));
